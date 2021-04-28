706d152eb63a76406af736c47e93152c6141221c
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2021-04-28 14:17:16 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,34 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("ExPack", "ruRU");
+if not L then return; end;
+
+--Addon ExPack
+L["ExPack by |cFF66ccffGhordian |rOptions"] = "ExPack от |cFF66ccffGhordian |rOptions";
+
+--Options
+L["Enable ExPack"] = "Включить ExPack";
+L["Enables/Disables ExPack."] = "Включает/отключает ExPack";
+L["Filters"] = "Фильтры";
+L["Enable Any Item"] = "Показать только предметы";
+L["Enable Reagents Only"] = "Показать только реагенты";
+L["Enable Banks Only"] = "Показать только банк";
+L["Reports"] = "Отчеты";
+L["Report errors to chat window"] = "Показывать ошибки в окне чата.";
+L["Report debug tracing to chat window"] = "Сообщить о трассировке отладки в окно чата.";
+L["Reset ExPack (Reloads UI after)"] = "Сбросить ExPack (после перезагрузки пользовательского интерфейса)";
+L["Commands"] = "Команды";
+L["/ep : Brings up this window."] = "/ep : Открывает это окно";
+
+--Expansions
+L["Unknown"] = "Неизвестно";
+L["Classic"] = "Classic";
+L["The Burning Crusade"] = "The Burning Crusade";
+L["Wrath of the Lich King"] = "Wrath of the Lich King";
+L["Cataclysm"] = "Cataclysm";
+L["Mists of Pandaria"] = "Mists of Pandaria";
+L["Warlords of Draenor"] = "Warlords of Draenor";
+L["Legion"] = "Legion";
+L["Battle for Azeroth"] = "Battle for Azeroth";
+L["Shadowlands"] = "Shadowlands";
+L["|cFFFF0000Battle |cFFE6CC80for |cFF2E6FF2Azeroth"] = true;
+
+--Chat Error Messages
